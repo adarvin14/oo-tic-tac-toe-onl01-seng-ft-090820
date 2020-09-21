be6d05785a49eabe3886d85d179d702ce28b0775
Alex Darvin <amdarvin14@gmail.com>
2020-09-21 21:58:42 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -113,15 +113,15 @@ end
    end
   end
   
-  #def play
-    until over?
-    turn
-  end
-   if combo = won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cat's Game!"
-    end
-  end
-end
+#   #def play
+#     until over?
+#     turn
+#   end
+#   if combo = won?
+#       puts "Congratulations #{winner}!"
+#     elsif draw?
+#       puts "Cat's Game!"
+#     end
+#   end
+# end
   
